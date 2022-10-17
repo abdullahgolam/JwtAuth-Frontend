a38cc6e3be9e424cb03a3ab935b0ae4d331e211b
Abdullah A. Golam <abdullahagolam@gmail.com>
2022-10-17 14:57:54 +0300

    update front end

--- JwtAuth/src/app/app.module.ts
+++ JwtAuth/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatToolbarModule,
     MatIconModule,
     MatTableModule,
+    FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- JwtAuth/src/app/login/login.component.ts
+++ JwtAuth/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  faUser = faUser
+  faLock = faLock
+
+  loginForm!: UntypedFormGroup;
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      }
+    );
+  }
+
+  onSubmit(): void {
+
+    this.http.post('http://localhost:8000/api/login', this.loginForm.getRawValue(), {
+      withCredentials: true
+    }).subscribe({
+      next: () => {
+        this.router.navigate(['../dashboard'])
+      },
+      error: () => {
+        alert('اسم المستخدم او كلمة المرور خاطئة !!!');
+      }
+    });
+
   }
 
 }
